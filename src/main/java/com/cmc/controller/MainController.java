3520f4a7d7f3b9f7639d894642a26fe7f238d9ae
nguyenanhtuan27101996 <nguyenanhtuan27101996@gmail.com>
2018-10-26 14:18:14 +0700

    update new perfect

--- src/main/java/com/cmc/controller/MainController.java
+++ src/main/java/com/cmc/controller/MainController.java
@@ -3,6 +3,8 @@
  */
 package com.cmc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @modifieddate Oct 26, 2018
  * @content ...
  */
+@Controller
 public class MainController {
 
         @GetMapping("/")
-        public String index() {
+        public String index(ModelMap modelMap) {
+               
                 return "index";
         }
 }
--- src/main/java/com/cmc/repository/HeDTRepository.java
+++ src/main/java/com/cmc/repository/HeDTRepository.java
@@ -3,12 +3,15 @@
  */
 package com.cmc.repository;
 
+import org.springframework.stereotype.Repository;
+
 /**
  * @author User
  * @creatdate Oct 26, 2018
  * @modifieddate Oct 26, 2018
  * @content ...
  */
-public interface HeDTRepository {
+@Repository
+public interface HeDTRepository{
 
 }
